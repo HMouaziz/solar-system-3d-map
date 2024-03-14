73837b6974ca166f020a568426b8c09cca0671ae
Vladimir Cascarade <vladimir.cascarade-ext@kering.com>
2024-03-14 16:03:28 +0000

    Add main styling 2

--- src/App.jsx
+++ src/App.jsx
@@ -40,14 +40,21 @@ function App() {
   return (
     <>
       <main className="app">
-        {!planetList && <Loader />}
-        <section className="appHeader">A 3D Mapper of The Solar System</section>
-        <section className="threeRenderer">
-          {planetList && <Canvas planetList={planetList} />}
-        </section>
-        <section className="appFooter">
-          Use your mouse to control the map.
-        </section>
+        {!planetList ? (
+          <Loader />
+        ) : (
+          <>
+            <section className="appHeader">
+              A 3D Mapper of The Solar System
+            </section>
+            <section className="threeRenderer">
+              {planetList && <Canvas planetList={planetList} />}
+            </section>
+            <section className="appFooter">
+              Use your mouse to control the map.
+            </section>
+          </>
+        )}
       </main>
     </>
   );
