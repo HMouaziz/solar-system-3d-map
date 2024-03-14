fbe35d389b3a731410da62163280a6d8542ab75c
hmoua <hmouaziz@project-hephaestus.com>
2024-03-14 14:37:29 +0000

    Stable Logic

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -1,49 +1,22 @@
 import * as THREE from 'three';
 import randomColor from 'randomcolor';
-import {useEffect, useRef} from "react";
-import {OrbitControls} from "three/addons";
-// import {TrackballControls} from "three/addons";
+import { useEffect, useRef } from "react";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-
-
-// eslint-disable-next-line react/prop-types
 const Canvas = ({planetList}) => {
     const mountRef = useRef(null);
 
     useEffect(() => {
-
-        const createPlanet = (size, color, distanceFromSun) => {
-            const geometry = new THREE.SphereGeometry(size, 32, 32);
-            const material = new THREE.MeshBasicMaterial({color: color});
-            const planet = new THREE.Mesh(geometry, material);
-            planet.position.set(distanceFromSun, 0,0);
-            return planet;
-        };
-
-        const createOrbitLine = (semimajorAxis, segments) => {
-            const points = [];
-            const orbitRadius = semimajorAxis * 0.000001;
-            for (let i = 0; i <= segments; i++) {
-                const angle = (i / segments) * 2 * Math.PI;
-                points.push(new THREE.Vector3(Math.cos(angle) * orbitRadius, 0, Math.sin(angle) * orbitRadius));
-            }
-
-            const geometry = new THREE.BufferGeometry().setFromPoints(points);
-            const material = new THREE.LineBasicMaterial({ color: 0xffffff, transparent: true, opacity: 0.5 });
-            const orbitLine = new THREE.LineLoop(geometry, material);
-
-            return orbitLine;
-        };
-
-
-
         const scene = new THREE.Scene();
         const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 10000);
         const renderer = new THREE.WebGLRenderer({antialias: true});
         renderer.setSize(window.innerWidth, window.innerHeight);
 
-        let controls = new OrbitControls(camera, renderer.domElement);
-        controls.listenToKeyEvents(window);
+
+        const controls = new OrbitControls(camera, renderer.domElement);
+        controls.minDistance = 0.5;  // Minimum zoom distance
+        controls.maxDistance = 100; // Maximum zoom distance
+        controls.enablePan = false;
 
         mountRef.current.appendChild(renderer.domElement);
 
@@ -52,30 +25,28 @@ const Canvas = ({planetList}) => {
         light.position.set(10, 10, 10);
         scene.add(light);
 
-        // Solar System Bodies
-        if (planetList) {
-            // eslint-disable-next-line react/prop-types
-            planetList.forEach((planet) => {
-                let coef = 0.000001;
-                let sun = 'Sun';
-                let distanceFromSun = planet.perihelion * coef;
-                let size = planet.englishName === sun ? planet.meanRadius * 0.0001 : planet.meanRadius * 0.001;
-                let color = planet.englishName === sun ? 0xffff00 : randomColor();
-                scene.add(createPlanet(size, color, distanceFromSun));
+        const scale = 0.00000001;
 
-                let semimajorAxis = planet.semimajorAxis;
-                scene.add(createOrbitLine(semimajorAxis, 100))
-            });
-        }
+        planetList?.forEach((planet) => {
+            const distanceFromSun = planet.semimajorAxis * scale;
+            const size = planet.englishName === "Sun" ? planet.meanRadius * scale * 30 : planet.meanRadius * scale * 1000;
+            const color = planet.englishName === "Sun" ? 0xffff00 : randomColor();
+            let inclination = planet.inclination;
+            let semimajorAxis = planet.semimajorAxis * scale;
 
-        // Adjust camera
-        camera.position.z = 1000;
+            const planetMesh = createPlanet(size, color, distanceFromSun);
+            planetMesh.userData.isPlanet = true;
+            scene.add(planetMesh);
+
+            const orbitLine = createOrbitLine(semimajorAxis, 100, inclination);
+            scene.add(orbitLine);
+        });
+
+        camera.position.z = 1;
 
         const animate = function () {
             requestAnimationFrame(animate);
-
             renderer.render(scene, camera);
-
             controls.update();
         };
 
@@ -84,7 +55,30 @@ const Canvas = ({planetList}) => {
         return () => {
             mountRef.current.removeChild(renderer.domElement);
         };
-    }, []);
+
+        function createPlanet(size, color, distanceFromSun) {
+            const geometry = new THREE.SphereGeometry(size, 32, 32);
+            const material = new THREE.MeshBasicMaterial({color: color});
+            const planet = new THREE.Mesh(geometry, material);
+            planet.position.set(distanceFromSun, 0, 0);
+            return planet;
+        }
+
+        function createOrbitLine(semimajorAxis, segments, inclination) {
+            const points = [];
+            for (let i = 0; i <= segments; i++) {
+                const angle = (i / segments) * 2 * Math.PI;
+                points.push(new THREE.Vector3(Math.cos(angle) * semimajorAxis, 0, Math.sin(angle) * semimajorAxis));
+            }
+
+            const geometry = new THREE.BufferGeometry().setFromPoints(points);
+            const material = new THREE.LineBasicMaterial({color: 0xffffff, transparent: true, opacity: 0.5});
+            const orbitLine = new THREE.LineLoop(geometry, material);
+            orbitLine.rotation.x = THREE.MathUtils.degToRad(inclination);
+
+            return orbitLine;
+        }
+    }, [planetList]);
 
     return <div className='canvas' ref={mountRef}></div>;
 };
